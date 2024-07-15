ed0ed0fd2d86f8451ff22d250a4a34a5d9c22e3c
DynnoOttu <dynnoottu968@gmail.com>
2024-07-15 20:52:41 +0700

    talent API

--- app/services/mongose/image.js
+++ app/services/mongose/image.js
@@ -15,4 +15,13 @@ const createImages = async (req) => {
   return result;
 };
 
-module.exports = { createImages, genereteImage };
+const checkingImage = async (id) => {
+  const result = await Images.findOne({ _id: id });
+  console.log(result);
+
+  if (!result) throw new NotFoundError(`Tidak ada Gambar dengan id :  ${id}`);
+
+  return result;
+};
+
+module.exports = { createImages, genereteImage, checkingImage };
