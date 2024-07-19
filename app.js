46eddada655f3b18d8dc77fa8391f196ecceb232
DynnoOttu <dynnoottu968@gmail.com>
2024-07-19 23:14:23 +0700

    CRUD API Events

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const categoriesRouter = require("./app/api/v1/categories/router");
 const imagesRouter = require("./app/api/v1/images/router");
 const talentsRouter = require("./app/api/v1/talents/router");
+const eventRouter = require("./app/api/v1/events/router");
 
 const v1 = "/api/v1/cms";
 
@@ -30,6 +31,7 @@ app.get("/", (req, res) => {
 app.use(v1, categoriesRouter);
 app.use(v1, imagesRouter);
 app.use(v1, talentsRouter);
+app.use(v1, eventRouter);
 
 app.use(notFoundMiddleware);
 app.use(handleErrorMiddleware);
