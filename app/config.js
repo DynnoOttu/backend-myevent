d0707ae2220ef60598ed6b0f342b648dd9d64fa6
Dynno Yohanis Ottu <dynnoottu968@gmail.com>
2024-08-25 21:45:52 +0700

    send email

--- app/config.js
+++ app/config.js
@@ -5,4 +5,6 @@ module.exports = {
   urlDb: process.env.URL_MONGODB_DEV,
   jwtExpiration: "24h",
   jwtSecret: process.env.JWT_SECRET,
+  email: process.env.EMAIL,
+  password: process.env.PASSWORD,
 };
